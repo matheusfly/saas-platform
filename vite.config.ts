d37fcd8101815fc1290bf8dd0d9a15def3fbb4e9
cyber-math <matheus.m.santos10@ba.estudante.senai.br>
2025-08-12 07:32:08 -0300

    Update UI testing suite

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: '.',
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+    headers: {
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    },
+  },
 })
